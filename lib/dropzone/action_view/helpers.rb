410b45ee6eac602f86d25686e2e8eaa47e969ab8
Tobias Strebitzer <tobias.strebitzer@gmail.com>
2013-11-19 23:09:57 +0100

    updated url generation

--- lib/dropzone/action_view/helpers.rb
+++ lib/dropzone/action_view/helpers.rb
@@ -3,7 +3,7 @@ module Dropzone
     module Helpers
   
       def dropzone(entity, profile)
-        url = dropzone_engine.url_for :controller => 'dropzone/profiles', :action => 'info', :format => :json, :profile_id => profile.to_s, :entity_id => entity.id
+        url = Dropzone::Engine.routes.url_for :controller => 'dropzone/profiles', :action => 'info', :only_path => true, :format => :json, :profile_id => profile.to_s, :entity_id => entity.id
         raw("<div class='dropzone-ui' data-url='#{url}'></div>")
       end
     end
